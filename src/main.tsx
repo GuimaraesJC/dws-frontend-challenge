4835fb7f6a2e5c6f886dce3933aeda99f817f72a
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-10 17:28:51 -0300

    feat: implements mobile-first grid system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import './styles/grid.css'
 import './styles/theme.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
