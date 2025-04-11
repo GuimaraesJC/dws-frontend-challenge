09198c4e2ebf467a126e8bebb683cce53c9d43e7
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-11 18:39:20 -0300

    refactor: updates store to receive posts and rename it

--- src/store/blogStore.ts
+++ src/store/blogStore.ts
@@ -1,32 +1,39 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
+import { Post } from '../types'
 
 type SortBy = 'asc' | 'desc'
 
-type InterfaceStoreState = {
+type BlogStoreState = {
+  // Data
+  posts: Post[]
+
   // UI State
   selectedAuthors: string[]
   selectedCategories: string[]
   sortBy: SortBy
 
   // Actions
+  setPosts: (posts: Post[]) => void
   setSelectedAuthors: (authors: string[]) => void
   setSelectedCategories: (categories: string[]) => void
   setSortBy: (sortBy: SortBy) => void
 }
 
-export const useInterfaceStore = create<InterfaceStoreState>()(
+export const useBlogStore = create<BlogStoreState>()(
   persist(
     (set) => ({
+      posts: [],
       selectedAuthors: [],
       selectedCategories: [],
       sortBy: 'asc',
 
+      setPosts: (posts) => set({ posts }),
       setSelectedAuthors: (authors) => set({ selectedAuthors: authors }),
       setSelectedCategories: (categories) => set({ selectedCategories: categories }),
       setSortBy: (sortBy) => set({ sortBy }),
     }),
     {
-      name: 'user-interface-store',
+      name: 'blog-store',
     })
 )
