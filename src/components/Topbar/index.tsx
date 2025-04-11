bd7aa29cbfba5ce735182373d77919e4d3a497e5
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-11 20:40:04 -0300

    feat: fixes Topbar positioning of icons

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -3,15 +3,17 @@ import styles from './Topbar.module.css'
 import SearchIcon from '../../assets/icons/search.svg?react'
 
 function Topbar() {
-	return (
-		<header className={styles.container}>
+  return (
+    <header className={styles.container}>
       <h1>DWS</h1>
+      <div className={styles.searchWrapper}>
       <input className={styles.input} type="text" placeholder="Search" />
       <button className={styles.button}>
         <SearchIcon className={styles.icon} />
       </button>
-		</header>
-	)
+      </div>
+    </header>
+  )
 }
- 
+
 export default Topbar
