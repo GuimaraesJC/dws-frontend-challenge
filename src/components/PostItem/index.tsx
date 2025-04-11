66c45c731c8ae6afa5cb971afd79e93bc1ffe11d
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-11 19:35:42 -0300

    feat: implements pixel perfect PostItem

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -16,8 +16,10 @@ function PostItem({ post: { author, content, title, thumbnail_url, createdAt, ca
           <span className={styles.createdAt}>{formatDate(createdAt)}</span>
           <span>{author.name}</span>
         </div>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.postDescription}>{content}</p>
+        <p className={styles.postContent}>
+          <h3 className={styles.postTitle}>{title}</h3>
+          <p className={styles.postDescription}>{content}</p>
+        </p>
       </main>
       <footer className={styles.footer}>
         {categories.map(category => (
