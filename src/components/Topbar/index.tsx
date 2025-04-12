6338ba69827299c71f2aa0539af59fb2b3c111e7
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-12 14:19:04 -0300

    feat: adds logo to Topbar

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,16 +1,20 @@
 import { IoMdSearch } from 'react-icons/io'
+import Logo from '../../assets/logo.png'
 import styles from './Topbar.module.css'
 
 
 function Topbar() {
   return (
     <header className={styles.container}>
-      <h1>DWS</h1>
+      <div className={styles.logoWrapper}>
+        <img className={styles.logo} src={Logo} alt="Dentsu World Services" />
+        <span className={styles.logoComplement}>world services</span>
+      </div>
       <div className={styles.searchWrapper}>
-      <input className={styles.input} type="text" placeholder="Search" />
-      <button className={styles.button}>
-        <IoMdSearch className={styles.icon} />
-      </button>
+        <input className={styles.input} type="text" placeholder="Search" />
+        <button className={styles.button}>
+          <IoMdSearch className={styles.icon} />
+        </button>
       </div>
     </header>
   )
