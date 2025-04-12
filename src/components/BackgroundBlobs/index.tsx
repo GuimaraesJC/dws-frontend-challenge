f2f43254a50fc2ee1470b079ede03137ef18bcbc
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-04-12 14:56:16 -0300

    feat: add blurred blobs on the background to match the design

---
+++ src/components/BackgroundBlobs/index.tsx
@@ -0,0 +1,12 @@
+import styles from './BackgroundBlobs.module.css'
+
+function BackgroundBlobs() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.blob1}></div>
+      <div className={styles.blob2}></div>
+    </div>
+  )
+}
+
+export default BackgroundBlobs
--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 
 import Topbar from '../../components/Topbar'
+import BackgroundBlobs from '../../components/BackgroundBlobs'
 
 function MainLayout() {
   return (
     <>
       <Topbar />
+      <BackgroundBlobs />
       <Outlet />
     </>
   )
